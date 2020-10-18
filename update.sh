c88e8b4cf302b4e694f46f1be14207f969a5d37c
root <root@HP-PC.localdomain>
2020-10-18 22:12:46 +0700

    add tweak(s)

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 dpkg-scanpackages ./debs/ > Packages
 sed -i -e '/^SHA/d' ./Packages
 bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: DNTmaster's Repo\nLabel: DNTmaster\nSuite: stable\nVersion: 1.0\nCodename: DNTmaster\nArchitecture: iphoneos-arm\nComponents: main\nDescription: DNTmaster's Tweaks\nmd5sum:\n "$(cat ./Packages | md5 -r | cut -d ' ' -f 1)" "$(stat -f%z Packages)" Packages\n "$(cat ./Packages.bz2 | md5 -r | cut -d ' ' -f 1)" "$(stat -f%z Packages.bz2)" Packages.bz2\n" >Release;
+printf "Origin: DNTmaster's Repo\nLabel: DNTmaster\nSuite: stable\nVersion: 1.0\nCodename: DNTmaster\nArchitecture: iphoneos-arm\nComponents: main\nDescription: DNTmaster's Tweaks\nmd5sum:\n "$(cat ./Packages | md5sum -c | cut -d ' ' -f 1)" "$(stat -f%z Packages)" Packages\n "$(cat ./Packages.bz2 | md5sum -c | cut -d ' ' -f 1)" "$(stat -f%z Packages.bz2)" Packages.bz2\n" >Release;
 python3 generate_packageinfo.py
 printf "\nUpdating 'latest tweaks' file"
 bash last_updates.sh
